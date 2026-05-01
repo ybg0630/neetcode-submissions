d8ce1638a4d8bcb671af1f044d2112ed0a4ac476
ybg0630 <95679837+ybg0630@users.noreply.github.com>
2026-05-01 16:59:07 +0800

    Add: python-if - submission-0

---
+++ Python For Beginners/python-if/submission-0.py
@@ -0,0 +1,10 @@
+def is_balance_low(balance: int) -> string:
+    if balance <= 100:
+        print("Warning: Low balance.")
+
+
+
+# do not modify below this line
+is_balance_low(99)
+is_balance_low(100)
+is_balance_low(101)
