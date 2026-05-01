b04be26d231b1be36cfdbe38705790bb7d4457e8
ybg0630 <95679837+ybg0630@users.noreply.github.com>
2026-05-01 14:56:29 +0800

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,13 @@
+integer_type = 5
+float_type = 5.2
+bool_type = False
+string_type = "asd"
+list_type = [2,4]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
