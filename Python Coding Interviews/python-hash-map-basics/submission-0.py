5201483b6623eae7945af5a0b03c783fc3867cd3
ybg0630 <95679837+ybg0630@users.noreply.github.com>
2026-05-03 21:25:54 +0800

    Add: python-hash-map-basics - submission-0

---
+++ Python Coding Interviews/python-hash-map-basics/submission-0.py
@@ -0,0 +1,26 @@
+from typing import List, Dict
+
+
+def build_hash_map(keys: List[str], values: List[int]) -> Dict[str, int]:
+    d = {}
+    for key,value in zip(keys, values):
+        d[key] = value
+    return d
+
+
+
+def get_values(hash_map: Dict[str, int], keys: List[str]) -> List[int]:
+    o = []
+    for key in keys:
+        o.append(hash_map[key])
+    return o
+
+
+# do not modify below this line
+print(build_hash_map(["Alice", "Bob", "Charlie"], [90, 80, 70]))
+print(build_hash_map(["Jane", "Carol", "Charlie"], [25, 100, 60]))
+print(build_hash_map(["Doug", "Bob", "Tommy"], [80, 90, 100]))
+
+print(get_values({"Alice": 90, "Bob": 80, "Charlie": 70}, ["Alice", "Bob", "Charlie"]))
+print(get_values({"Jane": 25, "Charlie": 60, "Carol": 100, }, ["Jane", "Carol"]))
+print(get_values({"X": 205, "Y": 78, "Z": 100}, ["Y"]))
