60f2d7f96371754a35256c4bbeb266d4caba1411
ybg0630 <95679837+ybg0630@users.noreply.github.com>
2026-05-15 11:26:56 +0800

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 4
+print(type(variable))
+variable = 4.2
+print(type(variable))
+variable = False
+print(type(variable))
+variable = ""
+print(type(variable))
+variable = [2]
+print(type(variable))
